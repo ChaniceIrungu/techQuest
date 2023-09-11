32401e4e7f5de56a778bf5c43ff85da7da3140ea
Chanice Irungu <chaniceashley@gmail.com>
2023-09-11 21:46:51 +0300

    minor card changes riddle

--- src/components/Riddles.vue
+++ src/components/Riddles.vue
@@ -15,7 +15,7 @@
       <div class="">
         <h1 class="font-extrabold text-purple-700 my-4">Find the RIDDLE</h1>
         <div
-          class="flip-card md:w-64 h-[250px] md:h-5/6 hover:scale-110"
+          class="flip-card md:w-64 h-[250px] md:h-5/6 hover:scale-110 shadow-xl"
           @click="flipCard"
         >
           <transition name="flip" mode="out-in">
