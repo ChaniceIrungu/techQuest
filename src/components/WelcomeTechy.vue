c3e1c8baf8cf2cb33dab281eae3f9d605b940702
Chanice Irungu <chaniceashley@gmail.com>
2023-09-12 09:55:30 +0300

    additional descriptive text

--- src/components/WelcomeTechy.vue
+++ src/components/WelcomeTechy.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="md:flex justify-center">
-    <div class="p-4">
-      <img :src="imageSrc" />
-    </div>
-
-    <div class="grid grid-cols place-items-center">
+    <div class="grid md:grid-cols-2 place-items-center px-4">
+      <div class="p-4">
+        <img :src="imageSrc" />
+      </div>
       <div class="text-center">
         <h1
-          class="pb-10 text-2xl text-center font-extrabold font-serif text-yellow-600"
+          class="md:pb-10 pb-4 md:text-2xl text-lg text-center font-extrabold font-serif text-yellow-600"
+        >
+          {{ HighlightMsg }}
+        </h1>
+        <h3
+          class="md:pb-10 pb-4 md:text-xl text-sm text-center font-extrabold font-serif text-yellow-600"
         >
           {{ message }}
-        </h1>
+        </h3>
 
         <button
-          class="bg-yellow-500 hover:bg-yellow-700 rounded py-4 px-20 text-white text-lg font-bold capitalize"
+          class="bg-yellow-500 hover:bg-yellow-700 rounded py-4 px-10 md:px-20 text-white text-lg font-bold capitalize"
           @click="onContinueClick"
         >
           <span class="">{{ state.buttonText }}</span>
@@ -27,8 +31,11 @@
 import { ref, reactive, computed } from "vue";
 import { useRouter } from "vue-router";
 
+const HighlightMsg = ref(
+  "Meet TechQuest!: A Free Gamified Tool to Unleash Your Skills and Find Your Dream Tech Career!"
+);
 const message = ref(
-  "TechQuest: Unleash Your Skills and Find Your Dream Tech Career!"
+  "Here the tool will assess your skills and preferences through a series of mini-games "
 );
 
 const state = reactive({
@@ -45,7 +52,8 @@ const router = useRouter();
 function onContinueClick() {
   if (state.buttonText === "CONTINUE") {
     state.image = "plainLogo";
-    message.value = "Complete the 2-minute Survey and Begin Your Journey!";
+    message.value =
+      "Complete the 2-minute Survey, Play Some Games, and Begin Your Journey!";
     state.buttonText = "Proceed";
   } else if (state.buttonText === "Proceed") {
     router.push("/survey");
