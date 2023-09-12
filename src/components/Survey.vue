0f7668421de457670e2ce72bac1a258365eb4ecb
Chanice Irungu <chaniceashley@gmail.com>
2023-09-12 18:43:29 +0300

    single-choice selection, multiple survey selection

--- src/components/Survey.vue
+++ src/components/Survey.vue
@@ -11,6 +11,7 @@
         </div>
       </div>
 
+      <mark>Checked items: {{ selectedChoices }}</mark>
       <!-- Survey goes here -->
       <div id="surv" class="bg-[#F3F3F3] border border-rounded-md my-4">
         <div class="p-4" v-if="currentQuestion">
@@ -26,24 +27,37 @@
               v-model="userInput"
             />
           </div>
-          <div v-else>
+          <div v-else class="cursor-pointer">
             <div
               class="py-2 text-left"
-              v-for="(choice, key) in currentQuestion.attributes.choices"
-              :key="key"
+              v-for="(choice, key, index) in currentQuestion.attributes.choices"
+              :key="index"
             >
-              <div
-                class="flex items-center space-x-2 border-2 rounded p-2 hover:bg-yellow-200"
+              <!-- <div
+                class="flex items-center space-x-2 border-2 rounded p-2 hover:bg-yellow-200 cursor-pointer"
               >
                 <input
-                  type="checkbox"
-                  :id="key"
+                  :type="
+                    currentQuestion.attributes.multiple ? 'checkbox' : 'radio'
+                  "
+                  :id="choice"
                   :value="choice"
                   v-model="selectedChoices"
-                  class="rounded-full text-blue-600 focus:ring-blue-400 h-4 w-4"
+                  class="rounded-full h-4 w-4"
                 />
-                <label :for="key" class="text-gray-800">{{ choice }}</label>
-              </div>
+                <label :for="choice" class="text-gray-800">{{ choice }}</label>
+              </div> -->
+
+              <label>
+                <input
+                  :type="
+                    currentQuestion.attributes.multiple ? 'checkbox' : 'radio'
+                  "
+                  :checked="isSelected(index)"
+                  @change="updateSelectedChoices(index)"
+                />
+                {{ choice }}
+              </label>
             </div>
           </div>
         </div>
@@ -64,7 +78,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 import { useRouter } from "vue-router";
 
 import axios from "axios";
@@ -74,8 +88,10 @@ const currentIndex = ref(0);
 const userInput = ref("");
 const currentQuestion = ref(null);
 const nextQuestionText = ref("Next Question");
-const selectedChoices = ref([]);
+const responses = ref([]);
 const router = useRouter();
+// Initialize selectedChoices as an array of arrays to store selections for each question
+const selectedChoices = ref([]);
 
 // Fetch the survey data
 onMounted(async () => {
@@ -86,11 +102,58 @@ onMounted(async () => {
   currentQuestion.value = surveys.value[currentIndex.value];
 });
 
+// Watch the currentIndex to ensure that selectedChoices is initialized for each question
+// watch(currentIndex, () => {
+//   // Initialize selectedChoices for the current question
+//   // if (currentIndex.value < surveys.value.length) {
+//   //   selectedChoices.value[currentIndex.value] = [];
+//   // }
+// });
+
+// Listen for changes in selectedChoices and save them for analysis
+// watch(selectedChoices, (newSelectedChoices) => {
+//   const question = currentQuestion.value.attributes.question;
+//   const selections = newSelectedChoices;
+
+//   // Perform analysis or save the question and selections as needed
+//   console.log("Question:", question);
+//   console.log("Selections:", selections);
+// });
+// Function to check if a choice is selected
+const isSelected = (index) => {
+  if (selectedChoices.value.length) {
+    return selectedChoices.value.includes(index);
+  }
+};
+
+// Function to update selectedChoices array
+const updateSelectedChoices = (index) => {
+  // const currentIndexValue = currentIndex.value;
+  const choices = selectedChoices.value;
+
+  // If it's a multiple-choice question, toggle selection
+  if (currentQuestion.value.attributes.multiple) {
+    const choiceIndex = choices.indexOf(index);
+    if (choiceIndex === -1) {
+      choices.push(index);
+    } else {
+      choices.splice(choiceIndex, 1);
+    }
+  } else {
+    // For single-choice questions, replace the selection
+    choices.length = 0; // Clear the array
+    choices.push(index); // Add the new selection
+  }
+};
 // Add a computed property to check if userInput is empty
 const shouldDisableNextButton = computed(() => {
   // Check if userInput is empty and the question type is 'input'
   return userInput.value.trim() === "";
 });
+
+const saveResponse = () => {
+  console.log("responses:", responses.value);
+};
 
 // Next and previous question handlers
 const nextQuestion = () => {
@@ -103,8 +166,19 @@ const nextQuestion = () => {
   }
 
   if (currentIndex.value <= surveys.value.length - 1) {
+    // Clear selectedChoices for the current question when moving to the next question
+    selectedChoices.value = [];
     currentIndex.value++;
     currentQuestion.value = surveys.value[currentIndex.value];
+    // Clear selectedChoices when moving to the next question
+    console.log("the question", currentQuestion.value);
+    console.log("selected choice:", selectedChoices.value);
+    // responses.value.push(selectedChoices.value);
+
+    // Clear selectedChoices for the current question when moving to the next question
+
+    // responses.value = [];
+    // selectedChoices.value = [];
   }
 
   if (currentIndex.value === surveys.value.length - 1) {
