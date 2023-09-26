bf3f22c31bfb4c77bfa81ab11103152cf6f4a56c
Chanice Irungu <chaniceashley@gmail.com>
2023-09-26 18:11:18 +0300

    chr:rounded-button lg

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded-full bg-gradient-to-r from-violet-500 to-fuchsia-500 hover:bg-gradient-to-r hover:from-violet-700 hover:to-fuchsia-700 py-3 px-10 text-white text-lg font-bold capitalize"
+    class="rounded-lg bg-gradient-to-r from-violet-500 to-fuchsia-500 hover:bg-gradient-to-r hover:from-violet-700 hover:to-fuchsia-700 py-3 px-10 text-white text-lg font-bold capitalize"
   >
     {{ buttonText }}
   </button>
