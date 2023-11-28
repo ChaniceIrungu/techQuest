378324cf837922b80057f75a795b8baaf8ea105d
Chanice Irungu <chaniceashley@gmail.com>
2023-11-29 02:07:28 +0300

    added pinia

---
+++ src/components/chart.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="font-bold">
+      Based on your results from the games these are the top roles
+    </div>
+    <div class="h-96 w-96"><Pie :data="data" :options="options" /></div>
+  </div>
+</template>
+
+<script setup>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "vue-chartjs";
+import { ref, defineProps, onMounted } from "vue";
+
+const { recommendedRolesArray, pieChartRoles } = defineProps([
+  "recommendedRolesArray",
+  "pieChartRoles",
+]);
+
+onMounted(async () => {
+  console.log("chart roles", recommendedRolesArray);
+});
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+const data = {
+  labels: pieChartRoles,
+  datasets: [
+    {
+      backgroundColor: [
+        "#41B883",
+        "#E46651",
+        "#00D8FF",
+        "#DD1B16",
+        "#f87979",
+        "#E46651",
+      ],
+      data: recommendedRolesArray,
+    },
+  ],
+};
+const options = {
+  responsive: true,
+  maintainAspectRatio: true,
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import "./assets/tailwind.css";
 
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+// Create a Pinia instance
+const pinia = createPinia();
+
 app.use(router);
+// Use Pinia instance in the app
+app.use(pinia);
 
 app.mount("#app");
